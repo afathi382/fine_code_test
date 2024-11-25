cf8c8baa9560ed00cfcae661027c6f686e0059f1
afathi382 <afathi382@gmail.com>
2024-11-25 15:18:21 +0330

    put

--- task/views/task_view.py
+++ task/views/task_view.py
@@ -8,6 +8,7 @@ from task.serializers.task_serializer import TaskSerializer
 
 class TaskAPIView(APIView):
     permission_classes = [IsAuthenticated]
+    serializer_class= [TaskSerializer]
 
     def get(self, request):   
 
@@ -17,4 +18,33 @@ class TaskAPIView(APIView):
 
 
         return Response(data=data, status=status.HTTP_200_OK)
+    
+    def post(self, request):   
+
+        serializer= TaskSerializer(data= request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.validated_data['user']= request.user
+        serializer.save()
+
+
+        return Response(status=status.HTTP_201_CREATED)
+    
+
+
+    def put(self, request):   
+
+        try:
+            task_id= request.data['task_id']
+            task= Task.objects.get(id= task_id)
+        except KeyError:
+            return Response(data={"message": 'task_id is requierd'}, status=status.HTTP_400_BAD_REQUEST)
+        except Task.DoesNotExist:
+            return Response(data={"message": 'task does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer= TaskSerializer(task, data= request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+
+        return Response(status=status.HTTP_201_CREATED)
     
